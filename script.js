2733b0316bd06f3583ef900153c3c7dfe1cc1b07
sourovmmoysanju <sourovmmoysanju@gmail.com>
2025-08-25 10:31:44 +0600

    add login from and add event for log in

---
+++ script.js
@@ -0,0 +1,15 @@
+document
+  .getElementById("lonin-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const mobNum = "01742818496";
+    const fixPin = "1234";
+    const inputMobNum = document.getElementById("mobile-input").value;
+    const inputPinNum = document.getElementById("pin-input").value;
+    if (inputMobNum === mobNum && inputPinNum === fixPin) {
+      console.log("ok");
+      window.location.href = "./home.html";
+    } else {
+      alert("Invalid Mobile Number or Pin Number");
+    }
+  });
