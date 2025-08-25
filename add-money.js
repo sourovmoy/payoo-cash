8945da0f904c9a7a40c92aed23bf83e8dc584f88
sourovmmoysanju <sourovmmoysanju@gmail.com>
2025-08-25 15:19:30 +0600

    add add money.js file

--- add-money.js
+++ add-money.js
@@ -2,8 +2,23 @@ document
   .getElementById("add-money-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    const accountBalance = document.getElementById("account-money").innerText;
-    console.log(accountBalance);
-    const enterAmount = document.getElementById("enter-amount").value;
-    console.log(enterAmount);
+    const accountBalance = parseInt(
+      document.getElementById("account-money").innerText
+    );
+    const enterAmount = parseInt(document.getElementById("enter-amount").value);
+
+    const accountNumber = document.getElementById("enter-account").value;
+    const fixedPin = 1234;
+    const enterPin = parseInt(document.getElementById("enter-pin").value);
+
+    if (accountNumber.length !== 11) {
+      alert("Please enter a valid 11 digit account number");
+      return;
+    }
+    if (enterPin !== fixedPin) {
+      alert("Please enter a valid 4 digit pin number");
+      return;
+    }
+    const totalAmount = accountBalance + enterAmount;
+    document.getElementById("account-money").innerText = totalAmount;
   });
